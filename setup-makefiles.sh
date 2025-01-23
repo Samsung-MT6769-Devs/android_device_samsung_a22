0d2d9624f365649f209667440c8f15f107c1c105
SamsungMT6769-Dev <davdavid7xw@gmail.com>
2025-01-23 18:20:15 +0200

    Initial skeleton

---
+++ setup-makefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=a22
+export DEVICE_COMMON=a22-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
